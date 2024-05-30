5f3e68640bf48580de6b71f3be2156dbcb1ba44c
BlastTea <arya.jbr.999@gmail.com>
2024-05-30 16:16:03 +0700

    feat: migrations

---
+++ database/migrations/2024_05_06_145221_create_transaction_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tour_package_id')->constrained();
+            $table->foreignId('transaction_id')->constrained();
+            $table->string('name', 100);
+            $table->double('price');
+            $table->integer('amount_of_people')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+}
